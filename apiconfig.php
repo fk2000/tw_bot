3c63fb3f68759f448a3601aeb3228495ac8db1e2
= <=>
2016-09-05 09:41:29 +0900

    first create

---
+++ apiconfig.php
@@ -0,0 +1,6 @@
+<?php
+define('API_KEY', '1onnwJn2s5mnDcUqBwpFhkHTU');
+define('API_SECRET', 'hntfRB7uQJOQr1x35a2gp6P7N4JOumT9MtMJ1c9H5RD4mq6YSn');
+define('ACCESS_TOKEN', '146778563-FVW2ziAYPpwrLqxxu5cDNWFRcR88hJLepX15DOZ7');
+define('ACCESS_SECRET', 'iVNPYbz3ws3f3u5KqSpTx6e3lMDpZl3pBMbpE2LsTuWt3');
+?>
---
+++ bot.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once('vendor/autoload.php');
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+require_once('apiconfig.php');
+
+$consumer_key = API_KEY;
+$consumer_secret = API_SECRET;
+$access_token = ACCESS_TOKEN;
+$access_token_secret = ACCESS_SECRET;
+
+$twObj = new TwitterOAuth(
+    $consumer_key,
+    $consumer_secret,
+    $access_token,
+    $access_token_secret
+);
+
+$list = array(
+    'Hello!',
+    'done!',
+);
+
+//$header = 'Content-Type: text/plain; charset=utf-8';
+//try {
+//    $to = new TwistOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
+
+//    $status = $to->post('statuses/update', ['status' => $list[array_rand($list)]]);
+//    header($header, true, 200);
+//    echo "tweet done!: https://twitter.com/{$status->user->screen_name}/status/{$status->id_str}\n";
+//} catch (TwistException $e) {
+//    header($header, true, $e->getCode() ?: 500);
+//    echo "tweet NG!: {$e->getMessage()}\n";
+//}
+$key = "コワーキング OR スタートアップ OR ベンチャー";
+
+//オプション設定
+$options = array('q'=>$key,'count'=>'1','lang'=>'ja');
+//array_toLog($options);
+
+//検索
+$json = $twObj->OAuthRequest(
+    'https://api.twitter.com/1.1/search/tweets.json',
+    'GET',
+    $options
+);
+$jset = json_decode($json, true);
+//tweetidを取得
+foreach ($jset['statuses'] as $result) {
+    $id = $result['id'];
+}
+//公式RT投稿
+echo $twObj->OAuthRequest(
+    'https://api.twitter.com/1.1/statuses/retweet/'.$id.'.json',
+    'POST',
+    array()
+);
+
+//log
+function toLog($str){
+    $filename = "log.txt";
+    $fp = fopen($filename,"a");
+    fputs($fp,$str);
+    fclose($fp);
+}
+//array_toLog
+function array_toLog($array_str){
+    $filename = "log.txt";
+    $fp = fopen($filename,"a");
+    foreach($array_str as $a){
+    fputs($fp,$a."\n");
+    }
+    fclose($fp);
+}
